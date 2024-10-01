5681a44a93165085e3c7a8b90b2092510065ba20
gaganpreet11 <gaganpreet.kaur@credresolve.com>
2024-10-01 19:47:33 +0530

    portfolio

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Gaganpreet Kaur </span>
+            from <span className="purple"> Punjab, India.</span>
+            <br />
+            I am currently a 4th year student at Chitkara University
+            <br />
+                        <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Reading Books
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Gagan</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
